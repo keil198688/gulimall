c9cc8f35983cde2bce6eb90db5998df48b93178f
keil <707294403@qq.com>
2020-12-26 19:20:58 +0800

    测试master

--- gulimall-master/gulimall-auth-server/src/main/java/gulimall/auth/config/MySpringSessionConfig.java
+++ gulimall-master/gulimall-auth-server/src/main/java/gulimall/auth/config/MySpringSessionConfig.java
@@ -27,7 +27,7 @@ public class MySpringSessionConfig {
         DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
         cookieSerializer.setCookieName("GULISESSION");
         cookieSerializer.setCookiePath("/");
-        //设置存储cookie的域(保证在各个子域名下都可以访问)测试合并
+        //设置存储cookie的域(保证在各个子域名下都可以访问)测试合并master
         cookieSerializer.setDomainName("gulimall.com");
         return cookieSerializer;
     }
