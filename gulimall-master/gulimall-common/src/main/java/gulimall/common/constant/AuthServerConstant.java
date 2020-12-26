8edcf472541d0f8a542e935d7358f0d90817ac59
keil <707294403@qq.com>
2020-12-26 21:26:30 +0800

    测试010111

--- gulimall-master/gulimall-common/src/main/java/gulimall/common/constant/AuthServerConstant.java
+++ gulimall-master/gulimall-common/src/main/java/gulimall/common/constant/AuthServerConstant.java
@@ -7,7 +7,7 @@ package gulimall.common.constant;
  */
 public class AuthServerConstant {
     /**
-     * 手机号缓存前缀测试01
+     * 手机号缓存前缀测试01011
      */
     public static final String SMS_CODE_CACHE_PREFIX = "sms:code:";
     /**
